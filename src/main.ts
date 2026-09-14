ba9e785bb1f4b0b841170d1ee66efbf704c40d6f
tnmyk <tanmaykachroo@gmail.com>
2026-09-15 03:37:24 +0530

    feat: show pinned tabs in separate row above tab bar
    Pin state detected via .mod-pinned status icon; CSS-only two-row layout
    (flex-wrap + order + separator) keeps headers as direct children so
    Obsidian's updateTabDisplay cannot strip them. Bar grows to fit both
    rows via :has(.mod-pinned).

--- src/main.ts
+++ src/main.ts
@@ -1,114 +1,58 @@
-import {
-	Editor,
-	MarkdownView,
-	MarkdownFileInfo,
-	Modal,
-	Notice,
-	Plugin,
-} from 'obsidian';
+import { Notice, Plugin } from 'obsidian';
+import { PinnedTabsRowManager } from './pinnedTabsRow';
 import {
 	DEFAULT_SETTINGS,
-	MyPluginSettings,
-	SampleSettingTab,
+	PinnedTabsRowSettings,
+	PinnedTabsRowSettingTab,
 } from './settings';
 
-// Remember to rename these classes and interfaces!
-
-export default class MyPlugin extends Plugin {
-	settings!: MyPluginSettings;
+export default class PinnedTabsRowPlugin extends Plugin {
+	settings!: PinnedTabsRowSettings;
+	pinnedTabsRow!: PinnedTabsRowManager;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		this.addRibbonIcon('dice', 'Sample', (_evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		this.pinnedTabsRow = new PinnedTabsRowManager(this);
+		this.pinnedTabsRow.start();
+		this.pinnedTabsRow.setEnabled(this.settings.enabled);
+
+		this.app.workspace.onLayoutReady(() => {
+			new Notice('Pinned tabs row loaded');
 		});
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status bar text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-modal-simple',
-			name: 'Open modal (simple)',
+			id: 'toggle-pin-current-tab',
+			name: 'Pin or unpin current tab',
 			callback: () => {
-				new SampleModal(this.app).open();
+				const leaf = this.app.workspace.getLeaf(false);
+				if (leaf) leaf.togglePinned();
 			},
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
-			editorCallback: (
-				editor: Editor,
-				_ctx: MarkdownView | MarkdownFileInfo,
-			) => {
-				editor.replaceSelection('Sample editor command');
-			},
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-modal-complex',
-			name: 'Open modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView =
-					this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-				return false;
+			id: 'debug-tab-row',
+			name: 'Debug tab row',
+			callback: () => {
+				this.pinnedTabsRow.debug();
 			},
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(activeDocument, 'click', (_evt: MouseEvent) => {
-			new Notice('Click');
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(
-			window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000),
-		);
+		this.addSettingTab(new PinnedTabsRowSettingTab(this.app, this));
 	}
 
-	onunload() {}
+	onunload() {
+		this.pinnedTabsRow.destroy();
+	}
 
 	async loadSettings() {
 		this.settings = Object.assign(
 			{},
 			DEFAULT_SETTINGS,
-			(await this.loadData()) as Partial<MyPluginSettings>,
+			(await this.loadData()) as Partial<PinnedTabsRowSettings>,
 		);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
 	}
 }
--- src/settings.ts
+++ src/settings.ts
@@ -1,18 +1,18 @@
-import { App, PluginSettingTab, Setting } from 'obsidian';
-import MyPlugin from './main';
+import { App, PluginSettingTab, Setting, SettingDefinitionItem } from 'obsidian';
+import PinnedTabsRowPlugin from './main';
 
-export interface MyPluginSettings {
-	mySetting: string;
+export interface PinnedTabsRowSettings {
+	enabled: boolean;
 }
 
-export const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default',
+export const DEFAULT_SETTINGS: PinnedTabsRowSettings = {
+	enabled: true,
 };
 
-export class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+export class PinnedTabsRowSettingTab extends PluginSettingTab {
+	plugin: PinnedTabsRowPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: PinnedTabsRowPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -23,16 +23,36 @@ export class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Settings #1')
-			.setDesc("It's a secret")
-			.addText((text) =>
-				text
-					.setPlaceholder('Enter your secret')
-					.setValue(this.plugin.settings.mySetting)
+			.setName('Pinned tabs row')
+			.setDesc('Show pinned tabs in a separate row above the tab bar.')
+			.addToggle((toggle) =>
+				toggle
+					.setValue(this.plugin.settings.enabled)
 					.onChange(async (value) => {
-						this.plugin.settings.mySetting = value;
+						this.plugin.settings.enabled = value;
 						await this.plugin.saveSettings();
+						this.plugin.pinnedTabsRow.setEnabled(value);
 					}),
 			);
+	}
+
+	getSettingDefinitions(): SettingDefinitionItem[] {
+		return [
+			{
+				name: 'Pinned tabs row',
+				desc: 'Show pinned tabs in a separate row above the tab bar.',
+				control: {
+					type: 'toggle',
+					key: 'enabled',
+				},
+			},
+		];
+	}
+
+	async setControlValue(key: string, value: unknown): Promise<void> {
+		await super.setControlValue(key, value);
+		if (key === 'enabled') {
+			this.plugin.pinnedTabsRow.setEnabled(Boolean(value));
+		}
 	}
 }
